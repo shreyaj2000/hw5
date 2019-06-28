1d9edb6e52014cbb82a0e85eeaa799816d0ab50e
shreya <shreya.sharad.jadhav@gmail.com>
2019-06-28 13:47:02 +0900

    Changes

--- python27/main.py
+++ python27/main.py
@@ -29,16 +29,41 @@ class Root(webapp2.RequestHandler):
 # このRequestHandlerでパタトカシーーのリクエストを処理して、結果を返す。
 class Pata(webapp2.RequestHandler):
     def get(self):
+        a = self.request.get('a', '')
+        b = self.request.get('b', '')
+        pata = ''
+        if len(a)>len(b):
+            length = len(a)
+        elif len(b)>len(a):
+            length = len(b)
+        else:
+            length = len(a)
+
+        for x in range(length):
+            pata += a[x:x+1] + b[x:x+1]
         # とりあえずAとBをつなぐだけで返事を作っていますけど、パタタコカシーーになるように自分で直してください！
-        pata = self.request.get("a") + self.request.get("b")
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
         # テンプレートの内容を埋め込んで、返事を返す。
         self.response.write(pataTmpl.render(pata=pata, request=self.request))
 
 class Norikae(webapp2.RequestHandler):
+
     def get(self):
+        departure = self.request.get('departure', '')
+        arrival = self.request.get('arrival', '')
+        next= False
+        transfer = []
+"""
+        for line in network:
+            for station in line["Stations"]:
+                if departure == station:
+                    transfer.append(line["Name"])
+                if arrival == station:
+                    break
+"""
         self.response.headers['Content-Type'] = 'text/html; charset=UTF-8'
-        self.response.write(networkTmpl.render(network=network))
+        self.response.write(networkTmpl.render(request=self.request,network=network))
+
 
 app = webapp2.WSGIApplication([
     ('/', Root),
